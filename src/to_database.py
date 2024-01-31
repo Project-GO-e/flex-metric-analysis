aa22effd74d832388be4fe77e6dc93d893a3f089
Jorrit Nutma <jorrit.nutma@tno.nl>
2024-01-31 10:05:26 +0100

    Write EV flex metrics per area to db

--- src/to_database.py
+++ src/to_database.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 from db.flex_metrics import Base, FlexMetrics
+from experiment_description import DeviceType, ExperimentDescription
 from experiment_filter import ExperimentFilter
 from experiment_loader import FileLoader
 
@@ -15,18 +16,22 @@ SHIFTED_DIR=BASE_DIR + 'ev/shifted/'
 
 DAY = datetime(2020,6,6)
 
-load_filter = ExperimentFilter()
-all_experiments = FileLoader(baselines_dir=Path(BASELINES_DIR), shifted_dir=Path(SHIFTED_DIR)).load_experiments(load_filter)
-
 engine = create_engine("sqlite:///test.db", echo=True)
 
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+FlexMetrics.metadata.drop_all(engine)
+FlexMetrics.metadata.create_all(engine)
 
+areas = set(map(lambda e: ExperimentDescription(e.stem, DeviceType.EV).get_area(), Path(BASELINES_DIR).iterdir()))
 
-with Session(engine) as session:
-    
-    for e in all_experiments.exp.values():
-        session.add(e.to_db_object())
-    session.commit()
+print(areas)
+
+for area in areas:
+
+    load_filter = ExperimentFilter().with_area(area)
+    all_experiments = FileLoader(baselines_dir=Path(BASELINES_DIR), shifted_dir=Path(SHIFTED_DIR)).load_experiments(load_filter)
+
+    with Session(engine) as session:
+        for e in all_experiments.exp.values():
+            session.add(e.to_db_object())
+        session.commit()
 
