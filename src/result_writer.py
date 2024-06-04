b526c4e8ab94fe6d3985194977f4c4533fe30bc7
Jorrit Nutma <jorrit.nutma@tno.nl>
2024-06-04 15:16:44 +0200

    Write results to directory

--- src/result_writer.py
+++ src/result_writer.py
@@ -1,12 +1,17 @@
 
+import datetime
+from pathlib import Path
 from typing import List
 from experiment.device_type import DeviceType
 from flex_metrics_results import Results
 
 
 class DirectoryResultWriter():
+    base_dir: Path = Path("results")
 
     def __init__(self, results: Results, reduce_group: List[DeviceType]) -> None:
+        if not DirectoryResultWriter.base_dir.exists():
+            DirectoryResultWriter.base_dir.mkdir()
         self.reduce_group = reduce_group
         self.results = results
 
@@ -17,7 +22,6 @@ class DirectoryResultWriter():
             if len(columns_to_reduce.columns) > 0:
                 results.drop(list(columns_to_reduce), axis=1, inplace=True)
                 results["flex_" + str(device_type).lower()] = columns_to_reduce.sum(axis=1)
-
         results['baseline'] = self.results.baselines().sum(axis=1)
-
-        results.round(1).to_csv('out.csv', sep=';')
+        res_file_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+        results.round(1).to_csv(DirectoryResultWriter.base_dir / (res_file_name + '.csv'), sep=';')
